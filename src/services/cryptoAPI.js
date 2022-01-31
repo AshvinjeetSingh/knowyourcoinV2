79d9a1e693733db421a80dc962e9c7f6dbf712c8
Ashvinjeet-singh <ashvinjeetsingh98@gmail.com>
2022-01-31 22:37:40 +0530

    chart value API fixed

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -11,7 +11,6 @@ export const cryptoApi= createApi({
   reducerPath:'cryptoApi',
   baseQuery: fetchBaseQuery({baseUrl: process.env.REACT_APP_CRYPTO_RAPIDAPI_BASEURL}
   ),
-  refetchOnFocus: true,
   endpoints: (builder)=>({
     getCryptos: builder.query({
       query: (count)=>createRequest(`/coins?limit=${count}`)
@@ -26,7 +25,7 @@ export const cryptoApi= createApi({
       query: (coinId) => createRequest(`/coin/${coinId}`),
     }),
     getCryptoHistory: builder.query({
-      query: ({ coinId, timePeriod }) => createRequest(`coin/${coinId}/history?timeperiod=${timePeriod}`),
+      query: ({ coinId, timePeriod }) => createRequest(`coin/${coinId}/history?timePeriod=${timePeriod}`),
     }),
   })
 
